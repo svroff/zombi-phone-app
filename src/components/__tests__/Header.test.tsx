f37c81ea5f847be680e7584ebb9eda853df70f98
Sergi Vicente <97890918+svroff@users.noreply.github.com>
2025-06-04 13:08:14 +0200

    test: add Header component test

---
+++ src/components/__tests__/Header.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Header from '../Header';
+import React from 'react';
+
+describe('Header', () => {
+  it('renders phone number', () => {
+    render(
+      <Header
+        content={{
+          logo: <div>Logo</div>,
+          menu: <div>Menu</div>,
+          contact: <div>666 777 888</div>,
+        }}
+      />,
+    );
+    expect(screen.getByText('666 777 888')).toBeInTheDocument();
+  });
+});
